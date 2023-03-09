daadce4dab2d8eefcf10a338b7285c96dee676b7
saadahmednu <https://saadahmednu@gitlab.com>
2023-03-09 12:52:31 -0800

    updated shell script

--- easeIO-c.sh
+++ easeIO-c.sh
@@ -4,8 +4,8 @@
 
 FILENAME="FIR_Filter_EASEIO_Org"
 
-LLVMHOME=/home/saad/llvm # take this as input
-LLVMBUILD=/home/saad/llvm/build # take this as input  
+LLVMHOME=~/easeIO/llvm-9.0.1-build/ # take this as input
+LLVMBUILD=~/easeIO/llvm-9.0.1-build/build/ # take this as input  
 CODEBASE=/home/saad/easeIO/EaseIO-codebase # where original codes are located
 ORGEBASE=/home/saad/easeIO/EaseIO-codebase/Benchmarks/Originals # where original codes are located
 TARGET_FOLDER=/home/saad/easeIO/EaseIO-codebase/Benchmarks/Transformed # where transformed codes will go.
